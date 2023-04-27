6319dc14b18e512a8d3dcabcff2a74b379b95ffd
JingYu Ning <115336923+jrunkening@users.noreply.github.com>
2023-04-27 15:22:41 +0800

    Implement Squeezer

---
+++ gwink/__main__.py
@@ -0,0 +1,34 @@
+import numpy as np
+from numpy import linalg as la
+import scipy.constants as const
+
+
+class Squeezer:
+    """
+    The `Squeezer` is represented by the operator $\mathcal{S}(\sigma, \phi)$.
+
+    $\mathcal{S}(\sigma, \phi) = \mathcal{R}(\phi) \mathcal{S}(\sigma, \phi) \mathcal{R}(-\phi)$
+    $= \mathcal{R}_\phi \mathcal{S}_\sigma \mathcal{R}_\phi^\dagger$
+
+    Args:
+        * `self` (`Squeezer`): A squeezer.
+        * `squeezing_factor` (`float`): Squeezing factor in dB.
+        * `squeezing_angle,` (`float`): Squeezing angle in rad.
+        * `injection_loss` (`float`): The loss that combined all of the
+            input losses together into a single frequency-independent loss.
+    """
+
+    def __init__(
+        self,
+        squeezing_factor,
+        squeezing_angle,
+        injection_loss
+    ) -> None:
+        self.squeezing_factor = squeezing_factor / (20*np.log10(np.exp(1)))
+        self.squeezing_angle = squeezing_angle
+        self.injection_loss = injection_loss
+        self.transfer_coefficient_injection_loss = np.sqrt(1-self.injection_loss)
+
+
+if __name__ == "__main__":
+    Squeezer()
